b0a8475514c64b980fb3096cd4a4bfd5f03066ab
Lushenggang <lushg-tcxg@qq.com>
2019-11-20 19:58:18 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    <search-highlight content="一条大河你呀A你呀你A呀你呀A你呀你呀你呀你呀你呀as你呀AS" keyword="你呀">
-    </search-highlight>
+    <div class="container">
+      <div class="header">
+        <input type="text" v-model="keyword">
+        <button @click.stop="searchLast">上一个</button>
+        <button @click.stop="searchNext">下一个</button>
+      </div>
+      <search-highlight
+        class="search-highlight"
+        ref="search"
+        :content="content"
+        :keyword="keyword">
+      </search-highlight>
+    </div>
   </div>
 </template>
 
@@ -12,6 +23,40 @@ export default {
   name: 'app',
   components: {
     SearchHighlight
+  },
+  data () {
+    return {
+      keyword: '月',
+      content: `
+            春江花月夜
+                [唐] 张若虚
+      春江潮水连海平，海上明月共潮生。
+      滟滟随波千万里，何处春江无月明！
+      江流宛转绕芳甸，月照花林皆似霰；
+      空里流霜不觉飞，汀上白沙看不见。
+      江天一色无纤尘，皎皎空中孤月轮。
+      江畔何人初见月？江月何年初照人？
+      人生代代无穷已，江月年年望相似。
+      不知江月待何人，但见长江送流水。
+      白云一片去悠悠，青枫浦上不胜愁。
+      谁家今夜扁舟子？何处相思明月楼？
+      可怜楼上月徘徊，应照离人妆镜台。
+      玉户帘中卷不去，捣衣砧上拂还来。
+      此时相望不相闻，愿逐月华流照君。
+      鸿雁长飞光不度，鱼龙潜跃水成文。
+      昨夜闲潭梦落花，可怜春半不还家。
+      江水流春去欲尽，江潭落月复西斜。
+      斜月沉沉藏海雾，碣石潇湘无限路。
+      不知乘月几人归，落月摇情满江树。`,
+    }
+  },
+  methods: {
+    searchLast () {
+      this.$refs.search.searchLast()
+    },
+    searchNext () {
+      this.$refs.search.searchNext()
+    }
   }
 }
 </script>
@@ -23,7 +68,37 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  width: 60px;
+}
+body {
+  margin: 0;
+}
+
+</style>
+
+<style>
+
+.container {
+  max-width: 20rem;
+  height: 100vh;
+  box-sizing: border-box;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.header {
+  display: flex;
+  height: 40px;
+  align-items: center;
+  flex-shrink: 0;
+}
+
+.search-highlight {
+  flex: auto;
+  max-height: 20em;
+  white-space: pre-line;
+  overflow: auto;
+  line-height: 2em;
 }
 </style>
